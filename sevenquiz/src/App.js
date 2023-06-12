1ecb20912e5721fb3a27dc030596e1ac8e3d9a94
Douglas Araujo <testecliente207@gmail.com>
2023-06-12 12:56:06 -0300

    initilizacion of project

---
+++ sevenquiz/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Welcome from './components/Welcome/Welcome';
+
+
+function App() {
+  return (
+    <>
+      <Welcome />
+    </>
+  );
+}
+
+export default App;
---
+++ sevenquiz/src/components/Welcome/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div>
+        <h2>Seja bem-vindo!</h2>
+        <p>Clique no botao abaixo para comecar:</p>
+        <button>Iniciar</button>   
+        <img src="" alt=""/>
+    </div>
+  )
+}
+
+export default Welcome
