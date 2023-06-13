0d76d988b8c679ef5323dc17c62b18f2e7e6f4f1
Douglas Araujo <testecliente207@gmail.com>
2023-06-13 14:58:08 -0300

    something config about general style

--- sevenquiz/src/components/Welcome/Welcome.js
+++ sevenquiz/src/components/Welcome/Welcome.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Login from '../../img/login.jpg';
 
 const Welcome = () => {
   return (
-    <div>
+    <div className="p-5 pt-20 justify-items-center items-center flex flex-col">
         <h2>Seja bem-vindo!</h2>
         <p>Clique no botao abaixo para comecar:</p>
         <button>Iniciar</button>   
-        <img src="" alt=""/>
+        <img src={Login} alt="Login page"/>
     </div>
   )
 }
