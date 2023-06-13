fb7edf5f242964a5a80d2191f96e1b90876d9322
Douglas Araujo <testecliente207@gmail.com>
2023-06-13 15:14:39 -0300

    update

--- sevenquiz/src/components/Welcome/Welcome.js
+++ sevenquiz/src/components/Welcome/Welcome.js
@@ -3,7 +3,7 @@ import Login from '../../img/login.jpg';
 
 const Welcome = () => {
   return (
-    <div className="p-5 pt-20 justify-items-center items-center flex flex-col">
+    <div className=" p-20 justify-items-center items-center flex flex-col">
         <h2>Seja bem-vindo!</h2>
         <p>Clique no botao abaixo para comecar:</p>
         <button>Iniciar</button>   
--- sevenquiz/tailwind.config.js
+++ sevenquiz/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'general-back': "./img/background3.jpg",
+      }
+    },
   },
   plugins: [],
 }
