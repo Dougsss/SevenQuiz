2ba53efd49bd36f9cbd9c73e4a49e0cda04df354
Douglas Araujo <testecliente207@gmail.com>
2023-06-15 22:39:20 -0300

    resolve problems about iniciar button

--- sevenquiz/src/components/Welcome/Welcome.js
+++ sevenquiz/src/components/Welcome/Welcome.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { useContext } from 'react';
 import { QuizContext } from '../../context/quiz';
 import Login from '../../img/login.png';
-import Button from '../Button/Button';
+
 
 const Welcome = () => {
-  const quizState = useContext(QuizContext);
+  const [quizState, dispatch] = useContext(QuizContext);
 
-  console.log(quizState);
 
   return (
     <div className=" p-20 justify-items-center items-center flex flex-col">
         <div className=" flex flex-col justify-self-center gap-4 text-center m-2">
           <h2 className="font-extrabold text-2xl sm:text-5xl md:text-[80px]">Seja bem-vindo!</h2>
           <p className=" italic text-[10px] sm:text-xs md:text-base">Clique no botao abaixo para comecar:</p>
-          <Button />
+          <button
+            className=" self-center rounded-3xl border-2 border-purple-600 hover:bg-purple-600 w-1/4 sm:h-10 text-xs sm:text-base"
+            onClick={() => dispatch({type:"CHANGE_STATE"})}>
+            Iniciar
+          </button>
         </div>  
         <img src={Login} alt="Login page" className=" w-2/5"/>
     </div>
--- sevenquiz/src/context/quiz.js
+++ sevenquiz/src/context/quiz.js
@@ -1,9 +1,30 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import questions from '../data/questions';
+
+const STAGES = [ "Start", "Playing", "End" ]
+
+const initialState = {
+    gameStage: STAGES[0],
+    questions
+}
+
+const quizReducer = (state, action) => {
+    console.log(state, action);
+
+    switch(action.type) {
+        case "CHANGE_STAGE":
+            return state;
+
+        default:
+            return state;
+    }
+
+}
 
 export const QuizContext = createContext()
 
 export const QuizProvider = ({children}) => {
-    const value = { Name: "Vai Rolar"};
+    const value = useReducer(quizReducer, initialState);
 
     return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
 };
