60185c80db6a1b0002f4918027d1d12510de7fec
Douglas Araujo <testecliente207@gmail.com>
2023-06-26 12:05:12 -0300

    addicion of page's Login and perfil

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -16,7 +16,7 @@ const Welcome = () => {
             Desafio da Semana!
           </h2>
           <p className=" mt-1 sm:mt-4 italic text-sm sm:text-base md:text-base">
-            {/* Lição dos Jovens Jul-Ago-Set */} Teste seus conhecimentos!
+            {/* Periodo da licao vigente */} Teste seus conhecimentos!
           </p>
           <button
             className=" mt-10 self-center rounded-3xl border-2 border-purple-600 hover:bg-purple-600 hover:font-bold w-[70%] sm:w-[25%] h-8 sm:h-10 text-sm sm:text-base"
