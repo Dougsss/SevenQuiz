37813f03530e7e5cbb587340f211f7e54f4b3807
Douglas Araujo <testecliente207@gmail.com>
2023-06-29 18:24:31 -0300

    I made the responsivity in login's page

--- src/components/LoginPage/Login.js
+++ src/components/LoginPage/Login.js
@@ -4,11 +4,11 @@ import Password from "../../img/password.png";
 
 const Login = () => {
   return (
-      <div className=" w-[80%] h-[500px] mx-[10%] my-[15%] self-center flex flex-row">
-        <div className="w-[60%] h-full rounded-l-3xl bg-slate-300 p-2">
+      <div className=" w-[94%] sm:w-[80%] sm:h-[500px] mx-[3%] sm:mx-[10%] my-[25%] sm:my-[15%] self-center flex flex-col sm:flex-row ">
+        <div className=" w-full sm:w-[60%] h-full rounded-t-3xl sm:rounded-tr-none sm:rounded-l-3xl bg-slate-300 p-2">
           <img src={Password} alt="passwordImg" className=" py-[13%]"/>
         </div>
-        <div className="w-[40%] h-full bg-slate-100 text-center py-[15%] p-2 rounded-r-3xl">
+        <div className="w-full sm:w-[40%] h-full bg-slate-100 text-center py-[15%] p-2 rounded-b-3xl sm:rounded-bl-none sm:rounded-r-3xl">
           <p className="mb-10 text-slate-700 font-medium text-xl">Login your account</p>
           <li className="flex flex-col gap-5 items-center">
             <Link to={"/startQuiz"}
@@ -16,7 +16,7 @@ const Login = () => {
                 Sing-in
               </Link>
             <Link to={"/newUser"}
-              className="bg-gradient-to-l from-violet-500 to-fuchsia-500 hover:bg-gradient-to-r from-fuchsia-500 to-violet-500 rounded-3xl w-[80%] h-12 sm:h-10 p-2">
+              className="bg-gradient-to-l from-violet-500 to-fuchsia-500 hover:bg-gradient-to-r from-fuchsia-500 to-violet-500 rounded-3xl w-[80%] h-12 sm:h-10 p-2 mb-10 sm:mb-0">
                 Create New
             </Link>
               {/* <button className="bg-gradient-to-l from-violet-500 to-fuchsia-500 hover:bg-gradient-to-r from-fuchsia-500 to-violet-500 rounded-3xl w-[80%] h-12 sm:h-10">
