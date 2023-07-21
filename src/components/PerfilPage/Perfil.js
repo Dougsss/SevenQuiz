153de4a3a7204f2f7b20f2ff4eb51a3778316728
Douglas Araujo <testecliente207@gmail.com>
2023-07-21 13:24:43 -0300

    setting in nav component

--- src/components/PerfilPage/Perfil.js
+++ src/components/PerfilPage/Perfil.js
@@ -10,19 +10,16 @@ const Perfil = () => {
   return (
     <>
       <nav className="bg-gray-900">
-        <div className=" flex items-center font-medium justify-around">
-          <div className=" h-12 font-light first-letter:font-bold first-letter:bg-violet-900">
-            Se7en Quiz
+        <div className=" flex fle items-center font-medium justify-between px-5">
+          <div className=" p-2 h-12 font-light first-letter:font-bold lett first-letter:bg-violet-900 flex flex-row">
+            Se<p className=" font-extrabold text-violet-500">7</p>en Quiz
           </div>
           <ul className=" flex items-center gap-5">
-            <li>Perfil</li>
-            <li>Configuracao</li>
-            <li className="cursor-pointer" onClick={() => [signout(), navigate("/")]}>Sair</li>
+            <li className="cursor-pointer  hover:text-violet-300">Perfil</li>
+            <li className="cursor-pointer  hover:text-violet-300">Configuracao</li>
+            <li className="cursor-pointer text-white  hover:text-violet-500 border-violet-500 border hover:border-white p-1" onClick={() => [signout(), navigate("/")]}>Sair</li>
           </ul>
         </div>
-        {/* <div className="m-2 p-2 h-[50%} w-[30%] bg-gray-900 cursor-pointer text-center" onClick={() => [signout(), navigate("/")]}>
-          <h1>I want out of my perfil!</h1>
-        </div> */}
       </nav>
     </>
   )
