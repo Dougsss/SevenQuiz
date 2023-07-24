193c8abd7dc9dcb96aa1a6b8c8f11026329ccf2d
Douglas Araujo <testecliente207@gmail.com>
2023-07-24 13:29:24 -0300

    i made responsive nav!

--- src/components/PerfilPage/Perfil.js
+++ src/components/PerfilPage/Perfil.js
@@ -14,11 +14,13 @@ const Perfil = () => {
           <div className=" p-2 h-12 font-light first-letter:font-bold lett first-letter:bg-violet-900 flex flex-row">
             Se<p className=" font-extrabold text-violet-500">7</p>en Quiz
           </div>
-          <ul className=" flex items-center gap-5">
-            <li className="cursor-pointer  hover:text-violet-300">Perfil</li>
-            <li className="cursor-pointer  hover:text-violet-300">Configuracao</li>
-            <li className="cursor-pointer text-white  hover:text-violet-500 border-violet-500 border hover:border-white p-1" onClick={() => [signout(), navigate("/")]}>Sair</li>
-          </ul>
+          <div className=" md:static absolute bg-gray-900 md:min-h-fit min-[60vh] left-0 top-[5%] md:w-auto w-full flex items-center px-5">
+            <ul className=" flex md:flex-row flex-col md:items-center md:gap-5 gap-7">
+              <li className="cursor-pointer  hover:text-violet-300">Perfil</li>
+              <li className="cursor-pointer  hover:text-violet-300">Configuracao</li>
+              <li className="cursor-pointer text-white  hover:text-violet-500 border-violet-500 border hover:border-white rounded-md p-1" onClick={() => [signout(), navigate("/")]}>Sair</li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
