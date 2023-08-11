a9b217886d8e424a0d9fe90eee2ccc5f3a46f8a1
Douglas Araujo <testecliente207@gmail.com>
2023-08-11 12:52:59 -0300

    made the link to quiz page

--- src/components/PerfilPage/Perfil.js
+++ src/components/PerfilPage/Perfil.js
@@ -17,11 +17,11 @@ const Perfil = () => {
   return (
     <>
       <nav className="bg-gray-900">
-        <div className=" flex fle items-center font-medium justify-between px-5">
+        <div className=" flex items-center font-medium justify-between px-5">
           <div className=" p-2 h-12 font-light first-letter:font-bold lett first-letter:bg-violet-900 flex flex-row">
             Se<p className=" font-extrabold text-violet-500">7</p>en Quiz
           </div>
-          <div className=" md:static absolute bg-gray-900 md:min-h-fit min-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
+          <div id='nav-links' className=" md:static absolute bg-gray-900 md:min-h-fit min-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
             <ul className=" flex md:flex-row flex-col md:items-center md:gap-5 gap-7">
               <li className="cursor-pointer  hover:text-violet-300">Perfil</li>
               <li className="cursor-pointer  hover:text-violet-300">Configuracao</li>
@@ -33,6 +33,16 @@ const Perfil = () => {
           </div>
         </div>
       </nav>
+      <section className="flex flex-row gap-2 p-2">
+        <div className="bg-gray-900 opacity-70 rounded-md w-[50%] h-20 flex items-center justify-center"> Status do Participante </div>
+        <div className="bg-gray-900 opacity-70 rounded-md w-[50%] h-20 flex items-center justify-center"> Rank Trimestre </div>
+      </section>
+      <section className="px-2 w-[100%] h-auto">
+        <div className="z-0 bg-gray-900 opacity-70 rounded-md p-5 flex flex-col items-center justify-center">
+          <p className="z-10 font-extrabold text-white"> Let's go the next game!</p>
+          <Link to={"/startQuiz"} className="z-10 cursor-pointer hover:bg-white border border-white rounded-full text-white font-semibold hover:text-gray-900 w-20 h-20 m-5 flex items-center justify-center">Start</Link>
+        </div>
+      </section>
     </>
   )
 }
