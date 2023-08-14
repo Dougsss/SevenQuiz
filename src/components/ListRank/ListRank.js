e5ee80f1362d16a481be3957d02245685ea27880
Douglas Araujo <testecliente207@gmail.com>
2023-08-14 14:06:21 -0300

    made other two components for estatics to perfil

---
+++ src/components/ListRank/ListRank.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListRank = () => {
+  return (
+    <div>ListRank</div>
+  )
+}
+
+export default ListRank
---
+++ src/components/Podium/Podium.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Podium = () => {
+  return (
+    <div>Podium</div>
+  )
+}
+
+export default Podium
