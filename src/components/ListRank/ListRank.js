16a2414d72959c052d6b4ca0a66cdb0db570797e
Douglas Araujo <testecliente207@gmail.com>
2023-10-03 20:36:52 -0300

    i made the list Rank

--- src/components/ListRank/ListRank.js
+++ src/components/ListRank/ListRank.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
 const ListRank = () => {
+    // Suponha que você tenha um array de itens
+    const itens = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7"];
+    
   return (
     <div className="m-1 flex justify-center items-center">
-      <img className=" rounded-md h-80" src='https://excelforever.com.br/wp-content/uploads/2021/03/image.png'/>
+      <div className="container mx-auto py-2">
+      <div className="grid grid-cols-1 gap-1">
+        {itens.map((item, index) => (
+          <div
+            key={index}
+            className="bg-gray-500 p-4 rounded shadow-md hover:shadow-lg"
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+      {/* <img className=" rounded-md h-80" src='https://excelforever.com.br/wp-content/uploads/2021/03/image.png'/> */}
     </div>
   )
 }
