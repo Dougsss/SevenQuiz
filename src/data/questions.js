8eb5db531bdbc6bfd043e416049f90b53482cc9f
Douglas Araujo <testecliente207@gmail.com>
2023-11-10 23:17:58 -0300

    atualizacao do banco de dados local

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,49 +1,74 @@
 const data = [
     {
-        question: "Qual o nome e a idade da pessoa mais velha mencionada na Bíblia?",
+        question: "O que significa a palavra Remanescente?",
         options: [
-            "Enos, viveu 905 anos",
-            "Noé, viveu 990 anos",
-            "Matusalém, viveu 969 anos",
-            "Rainha Ester, viveu 859 anos",
+            "Primeiros",
+            "Original",
+            "Restante",
+            "Escolhidos",
         ],
-        answer: "Matusalém, viveu 969 anos",
+        answer: "Restante",
     },
     {
-        question: "Qual desses não teve o seu nome mudado na Bíblia?",
-        options: ["Sara", " Abraão", "Jacó", "Davi"],
-        answer: "Davi",
-    },
-    {
-        question: "Sobre Samuel, o que não é verdade?",
+        question: "Em Apocalipse 12 o que a mulher pura vestia?",
         options: [
-            "Sua mãe se chamava Ana",
-            "Ungiu 3 reis de Israel: José, Saul e Davi",
-            "Sucedeu o profeta Eli",
-            "Teve uma visão enquanto ainda era muito novo"
+            "Vestida de 12 estrelas com uma coroa de sol na cabeça e a lua debaixo dos pés",
+            "Vestida do sol com a lua debaixo dos pés e uma coroa de 7 estrelas",
+            "Vestida de escarlate purpura e coroa de pedras preciosas e perolas",
+            "Vestida do sol com a lua debaixo dos pés e uma coroa de 12 estrelas na cabeça"
         ],
-        answer: "Ungiu 3 reis de Israel: José, Saul e Davi",
+        answer: "Vestida do sol com a lua debaixo dos pés e uma coroa de 12 estrelas na cabeça",
     },
     {
-        question: "Em quais livros da Bíblia é narrada a história do nascimento de Jesus?",
-        options: ["Gênesis e Salmos", "Mateus e Marcos", "Hebreus e Apocalipse", "Mateus e Lucas"],
-        answer: "Mateus e Lucas",
+        question: "Em que ano e local foi formada a primeira associação?",
+        options: [
+            "1844 em Minnessota-EUA",
+            "1861 em Michigan-EUA",
+            "1863 em Nova York-EUA",
+            "1812 em Toronto-Canadá"
+        ],
+        answer: "1861 em Michigan-EUA",
+    },
+    {
+        question: "De acordo com a lição qual e a missão da igreja de Deus?",
+        options: [
+            "Ir a igraja",
+            "Guardar os mandamentos",
+            "Ser a maior instituição religiosa do mundo",
+            "Levar o evangelho a todo mundo"
+        ],
+        answer: "Levar o evangelho a todo mundo",
     },
     {
         question:
-        "Quantos casais humanos entraram na arca de Noé?",
-        options: ["4 casais", "1 casal", "6 casal", "3 casal"],
-        answer: "4 casais",
+        "Em I Coríntios 14 o que Paulo alega sobre o carácter de Deus?",
+        options: [
+            "Um Deus de paz e amor",
+            "Um Deus de amor e ordem",
+            "Um Deus de paz e ordem",
+            "Um Deus de justiça e ordem"
+        ],
+        answer: "Um Deus de paz e ordem",
     },
     {
-        question: "Qual o nome da cidade fundada por Caim, depois que saiu da presença de Deus?",
-        options: ["Sodoma", "Gomorra", "Enoque", "Canaã"],
-        answer: "Enoque",
+        question: "Conforme a lição, por quanto tempo Deus providenciou alimento para a sua igreja apos ela fugir para o deserto?",
+        options: [
+            "980 anos",
+            "76 semanas",
+            "1260 anos",
+            "2000 anos"
+        ],
+        answer: "1260 anos",
     },
     {
-        question: "Qual dos nomes a baixo nao  foi um apostolo?",
-        options: ["Tadeu", "Filipe", "Bartolomeu", "Marcos"],
-        answer: "Marcos",
+        question: "Qual a característica do povo de Deus nos últimos dias?",
+        options: [
+            "Ter o testemunho de Deus, e guardar os mandamentos de Jesus",
+            "Guardar o Sábado",
+            "Guardar os mandamentos de Deus e ter o testemunho de Jesus",
+            "Guardar o testemunho de Jesus"
+        ],
+        answer: "Guardar os mandamentos de Deus e ter o testemunho de Jesus",
     },
     ];
 
